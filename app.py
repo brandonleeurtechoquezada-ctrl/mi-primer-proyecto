f689b94fea2554674d69fc09132d90fb574e6d1b
burandori <bluq1112@hotmail.com>
2026-08-09 12:09:44 -0500

    primer commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def inicio():
+    titulo = 'Mi aplicación ejemplo'
+    items = [
+        { 'titulo': 'Productos', 'descripcion': 'Explora nuestra variedad de productos y encuentra lo que necesitas' },
+        { 'titulo': 'Ofertas', 'descripcion': 'Aprovecha nuestras ofertas exclusivas con precios especiales' },
+        { 'titulo': 'Contacto', 'descripcion': 'Estamos aquí para ayudarte. No dudes en contactarnos' }
+    ]
+    return render_template('index.html', titulo=titulo, elementos=items)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
