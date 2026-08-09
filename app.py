f405e17a45b0f33d7a351aa1c26691e93141fdc5
burandori <bluq1112@hotmail.com>
2026-08-09 13:41:09 -0500

    config: Uso de SQLAlchemy

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from extensions import db
 from models import Contacto
 
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
+db.init_app(app)
 
 
 
