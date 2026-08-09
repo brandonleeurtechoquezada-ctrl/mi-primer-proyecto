0b728095a2371c6d5816ac8e8eefa627face898d
burandori <bluq1112@hotmail.com>
2026-08-09 13:02:23 -0500

    func: Se agregaron dos vistas nuevas (crear y editar contacto)

--- app.py
+++ app.py
@@ -13,5 +13,15 @@ def inicio():
     return render_template('index.html', titulo=titulo, elementos=items)
 
 
+@app.route('/crear')
+def crear():
+    return render_template('crear.html')
+
+
+@app.route('/editar')
+def editar():
+    return render_template('editar.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
