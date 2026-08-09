8225bcbd9bdec3192a5af4266d5cab3385488fd4
burandori <bluq1112@hotmail.com>
2026-08-09 13:05:15 -0500

    refactor: se eliminó código antiguo

--- app.py
+++ app.py
@@ -4,13 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    titulo = 'Mi aplicación ejemplo'
-    items = [
-        { 'titulo': 'Productos', 'descripcion': 'Explora nuestra variedad de productos y encuentra lo que necesitas' },
-        { 'titulo': 'Ofertas', 'descripcion': 'Aprovecha nuestras ofertas exclusivas con precios especiales' },
-        { 'titulo': 'Contacto', 'descripcion': 'Estamos aquí para ayudarte. No dudes en contactarnos' }
-    ]
-    return render_template('index.html', titulo=titulo, elementos=items)
+    return render_template('index.html')
 
 
 @app.route('/crear')
