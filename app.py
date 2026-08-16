1576d37736dd156bcb33712b41e32a706a31897b
burandori <bluq1112@hotmail.com>
2026-08-16 10:44:35 -0500

    func: Implementar creación de contacto con formulario y redirección

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, url_for, redirect
 from extensions import db
 from models import Contacto
 
@@ -8,9 +8,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db.init_app(app)
 
+### ctrl + shift + o
 
-
-@app.route('/')
+@app.route('/') ### Esta es la ruta de visualización de todos los contactos
 def inicio():
     ### Se guarda en una variable contactos
     ### De la tabla contactos, consulta a todos los contactos y los guarda en esa variable
@@ -18,9 +18,29 @@ def inicio():
     return render_template('index.html', contactos = contactos)
 
 
-@app.route('/crear')
-def crear():
-    return render_template('crear.html')
+### Paso 1: Visualizar el formulario de la creación del contacto
+@app.route('/crear', methods = ['GET']) ### Este es un método de tipo de GET
+def ver_creacion_contacto():
+    return render_template('crear.html') ### Renderizame esta plantilla -> "crear.html"
+
+#### Tipos de métodos:
+##### - Para visualización normalmente los métodos se les conoce como: métodos GET (obtener data)
+##### - Para la creación normalmente los métodos se les conoce como: métodos POST (enviar data)
+
+### Paso 2: Enviar el formulario
+### Esto se va a ejecutar cuando de click en el botón "Crear contacto"
+@app.route('/crear', methods = ['POST'])  ### Este es un método de tipo POST
+def crear(): ### request = petición
+    ### parte 1: Formar el objeto
+    nuevo_contacto = Contacto(
+        nombre = request.form['nombre'],
+        telefono = request.form['telefono'],
+        email = request.form['email'],
+    )
+    ### parte 2: guardarlo en la base de datos
+    db.session.add(nuevo_contacto)
+    db.session.commit()
+    return redirect(url_for("inicio")) ### Aquí le estoy pidiendo que me redirija al listado de contactos, que es el inicio
 
 
 @app.route('/editar')
